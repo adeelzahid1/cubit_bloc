af22650bdc5da8fc3bce65ca50861a483e144747
adeelzahid1 <developer.adeelzahid@gmail.com>
2022-04-04 18:18:14 +0500

    cubit 2 cubit bloclistner

--- lib/cubits/counting/counting_cubit.dart
+++ lib/cubits/counting/counting_cubit.dart
@@ -1,44 +1,13 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
-import 'package:cubit_bloc/cubits/color/color_cubit.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 
 part 'counting_state.dart';
 
 class CountingCubit extends Cubit<CountingState> {
-  int incrementSize = 1;
-  final ColorCubit colorCubit;
-  late final StreamSubscription colorSubscription;
 
- CountingCubit({required this.colorCubit})
-  : super(CountingState.initial()){
-    colorSubscription = colorCubit.stream.listen((ColorState colorState) {
-      if(colorState.color == Colors.red){
-        incrementSize = 1;
-      }
-      else if(colorState.color == Colors.blue){
-        incrementSize = 10;
-      }
-      else if(colorState.color == Colors.green){
-        incrementSize = 100;
-      }
-      else if(colorState.color == Colors.black){
-       emit(state.copyWith(counting: state.counting - 100));
-       incrementSize = -100;
-      }
-    });
-  }
+ CountingCubit(): super(CountingState.initial()){}
 
-  void changeCounting(){
+  void changeCounting(int incrementSize){
     emit(state.copyWith(counting: state.counting + incrementSize));
   }
-
-
-  Future<void>close(){
-    colorSubscription.cancel();
-    return super.close();
-  }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,69 +14,92 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<ColorCubit>(create: (context) => ColorCubit(),),
-        BlocProvider<CountingCubit>(create: (context) => CountingCubit(
-          colorCubit: context.read<ColorCubit>(),
-           ),),
+        BlocProvider<ColorCubit>(
+          create: (context) => ColorCubit(),
+        ),
+        BlocProvider<CountingCubit>(
+          create: (context) => CountingCubit(),
+        ),
       ],
       child: MaterialApp(
-            title: 'Cubit to Cubit basics',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(primarySwatch: Colors.blue),
-            home: MyHomePage(),
+        title: 'Cubit to Cubit basics',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: MyHomePage(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int incrementSize = 1;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Cubit 2 Cubit Demo')),
-      backgroundColor: context.watch<ColorCubit>().state.color,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text(
-                'Change Color',
-                style: TextStyle(fontSize: 24.0),
+    return BlocListener<ColorCubit, ColorState>(
+      listener: (context, state) {
+         if (state.color == Colors.red) {
+          incrementSize = 1;
+        } else if (state.color == Colors.green) {
+          incrementSize = 10;
+        } else if (state.color == Colors.blue) {
+          incrementSize = 100;
+        } else if (state.color == Colors.black) {
+          context.read<CountingCubit>().changeCounting(-100);
+          incrementSize = -100;
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(title: Text('Cubit 2 Cubit Demo')),
+        backgroundColor: context.watch<ColorCubit>().state.color,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: Text(
+                  'Change Color',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+                onPressed: () {
+                  // context.read<ColorCubit>().changeColor();
+                  BlocProvider.of<ColorCubit>(context).changeColor();
+                },
               ),
-              onPressed: () {
-                // context.read<ColorCubit>().changeColor();
-                BlocProvider.of<ColorCubit>(context).changeColor();
-              },
-            ),
-            SizedBox(height: 20.0),
-            BlocBuilder<CountingCubit, CountingState>(
-              builder: (context, state) {
-                return Text(
-                  '${state.counting}',
-                  style: TextStyle(
-                    fontSize: 52.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                );
-              //  '${context.watch<CountingCubit>().state.counting}',
-              },
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              child: Text(
-                'Increment Counter',
-                style: TextStyle(fontSize: 24.0),
+              SizedBox(height: 20.0),
+              BlocBuilder<CountingCubit, CountingState>(
+                builder: (context, state) {
+                  return Text(
+                    '${state.counting}',
+                    style: TextStyle(
+                      fontSize: 52.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  );
+                  //  '${context.watch<CountingCubit>().state.counting}',
+                },
               ),
-              onPressed: () {
-                // context.read<CountingCubit>().changeCounting();
-                BlocProvider.of<CountingCubit>(context).changeCounting();
-              },
-            ),
-          ],
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text(
+                  'Increment Counter',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+                onPressed: () {
+                  // context.read<CountingCubit>().changeCounting();
+                  BlocProvider.of<CountingCubit>(context).changeCounting(incrementSize);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
